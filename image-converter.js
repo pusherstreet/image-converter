d1fbba3469e36852cdb1ee26b7784acdf57c5d43
Vitaliy Diachuk <VDiachuk@XKIEV>
2017-10-25 16:24:44 +0300

    fix small bug

--- image-converter.js
+++ image-converter.js
@@ -4,7 +4,7 @@ var image = document.getElementById('image');
 
 ctx.drawImage(image, 0, 0);
 
-var rgb = ctx.getImageData(0, 0, 1, 1);
+var rgb = ctx.getImageData(0, 0, 1, 1).data;
 var bright = 0.33 * rgb[0] + 0.5 * rgb[1] + 0.16 * rgb[2];
 
 console.log(bright);
