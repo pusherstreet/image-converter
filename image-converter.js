12a85f3fb77e3dc0e5daffcc27085bba3c966758
Vitaliy Diachuk <VDiachuk@XKIEV>
2017-10-26 15:43:21 +0300

    add happy face =D

--- image-converter.js
+++ image-converter.js
@@ -1,10 +1,25 @@
+
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
-var image = document.getElementById('image');
 
-ctx.drawImage(image, 0, 0);
+var brightArr = [];
 
-var rgb = ctx.getImageData(0, 0, 1, 1).data;
-console.log(rgb)
-var bright = 0.33 * rgb[0] + 0.5 * rgb[1] + 0.16 * rgb[2];
-console.log(bright);
+var image = new Image();
+image.onload = () => {
+    const width = image.width;
+    const height = image.height;
+    console.log(width)
+    
+    ctx.drawImage(image, 0, 0);
+    for(let i = 0; i < height; i++){
+        for(let j = 0; j < width; j++){
+            let rgb = ctx.getImageData(j, i, 1, 1).data;
+            var bright = Math.floor (0.33 * rgb[0] + 0.5 * rgb[1] + 0.16 * rgb[2]);
+            document.write(bright > 200 ? "&nbsp;" : "@");
+        }
+        document.write('</br>');
+    }
+}
+image.src = 'happy.jpg';
+
+
